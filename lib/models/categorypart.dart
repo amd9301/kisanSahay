cc7757a7fd5aff83adfcc7960ec5304b5927f2ae
Mounikadurga <mounika9301@gmail.com>
2021-07-09 11:15:47 +0530

    Third commit

---
+++ lib/models/categorypart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryPart{
+  String name;
+  String imgName;
+  bool isSelected=false;
+
+  CategoryPart({
+    required this.name,
+    required this.imgName,
+    required this.isSelected
+}){}
+
+}
